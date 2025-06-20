34c05b76530cb24d1a520565ac44f9fa484a12ba
villageworks <villageworks@gmail.com>
2025-06-21 01:02:55 +0900

    ver.0.10.00 (MLS112.2): 商品カテゴリコードをカテゴリ名に変換して表示する（DTOの導入版）

--- src/main/java/com/example/demo/controller/ItemConrtroller.java
+++ src/main/java/com/example/demo/controller/ItemConrtroller.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.dto.ItemDTO;
 import com.example.demo.entity.Item;
 import com.example.demo.repository.ItemRepository;
 
@@ -28,7 +30,17 @@ public class ItemConrtroller {
 	@GetMapping("/list")
 	public String index(Model model) {
 		// itemsテーブルからすべての商品の商品リストを取得
-		List<Item> list = itemRepository.findAll(Sort.by(Sort.Direction.ASC, "id"));
+		List<Item> itemList = itemRepository.findAll(Sort.by(Sort.Direction.ASC, "id"));
+		// 表示用ItemDTOを要素とする商品リストに詰替
+		List<ItemDTO> list = new ArrayList<ItemDTO>();
+		for (Item item : itemList) {
+			ItemDTO dto = new ItemDTO();
+			dto.setId(item.getId());
+			dto.setCategory(item.getCategoryName());
+			dto.setName(item.getName());
+			dto.setPrice(item.getPrice());
+			list.add(dto);
+		}
 		// 商品リストを共用のデータ置き場に登録
 		model.addAttribute("items", list);
 		// 画面遷移
