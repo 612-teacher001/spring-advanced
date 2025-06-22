dbd09403c5e1460cd5b0a01d4f68f6bc08f7727f
villageworks <villageworks@gmail.com>
2025-06-22 15:15:30 +0900

    ver.0.20.00 (MLS212): 都道府県コードを都道府県名に変換して表示する

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 import lombok.Data;
@@ -22,7 +24,9 @@ public class User {
 	private Integer id;
 	private Integer role;
 	private String name;
-	private String prefecture;
+	@ManyToOne
+	@JoinColumn(name = "prefecture")
+	private Prefecture prefecture;
 	private String address;
 	private String phone;
 	private String email;
